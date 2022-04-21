60967832fb8a984bf49cfa41bda445b017b77e97
Tenebrosful <hugobernard39@gmail.com>
2022-04-21 03:50:42 +0200

    Delete build

--- build/listeners/listener.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Listener {
-    constructor(bot) {
-        this.bot = bot;
-    }
-}
-exports.default = Listener;
