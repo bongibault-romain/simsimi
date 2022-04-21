ec7a82dfbc66f48ed88be368ceb99dbb5467ca87
Tenebrosful <hugobernard39@gmail.com>
2022-04-21 05:40:57 +0200

    Add /learn command

--- src/errors/learn/LearnError.ts
+++ src/errors/learn/LearnError.ts
@@ -1,11 +1,11 @@
-import { Message } from "discord.js";
+import { CommandInteraction, Message } from "discord.js";
 
 export default class LearnError extends Error {
   constructor(message: string) {
     super(message);
   }
 
-  async replyToUser(message: Message) {
+  async replyToUser(message: Message | CommandInteraction) {
     await message.reply(this.message);
   }
 }
