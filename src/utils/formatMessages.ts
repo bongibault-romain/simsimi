d9ff9112784be4ebaa40d3a7b8fa2ef807fdd0a4
Tenebrosful <hugobernard39@gmail.com>
2022-04-21 05:23:39 +0200

    Import hasNitroEmotes

--- src/utils/formatMessages.ts
+++ src/utils/formatMessages.ts
@@ -3,3 +3,7 @@ export function format(message: string, options?: { toLowerCase: boolean }) {
   if (options?.toLowerCase) res = res.toLowerCase();
   return res;
 }
+
+export function hasNitroEmotes(message: string) {
+  return message.indexOf(":") + 1 != message.lastIndexOf(":") + 1;
+}
